c8d3f51946a594e02bce69977b684f4621936408
Sofia Gonzalez <sofia@gmail.com>
2026-09-17 13:48:51 +0200

    Primera version del portal

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://nifxqamgdxobgtkhiomw.supabase.co";
+const SUPABASE_KEY = "sb_publishable_2V_qfvGT2GyCA0jINPlAgA_6FkZfEvH";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
